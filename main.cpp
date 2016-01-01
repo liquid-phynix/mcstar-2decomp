3df1dfc2aec501176b7b6d3c0903aeda3b5cfa43
Dexter Filmore <liquid.phynix@gmail.com>
2016-01-01 14:52:50 +0100

    now we can test

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@
 #include "utils.hpp"
 #include "array.hpp"
 #include "bookkeeping.hpp"
-#include "fft-fftw.hpp"
 #include "decomp.hpp"
+#include "dfft.hpp"
 
 #ifdef SINGLEFLOAT
 typedef float Float;
@@ -24,14 +24,6 @@ void init_array(const int& gi0, const int& gi1, const int& gi2, Float& v){ v = g
 //void init_array(const int& gi0, const int& gi1, const int& gi2, Float& v){ v = gi1; }
 //void init_array(const int& gi0, const int& gi1, const int& gi2, Float& v){ v = gi2; }
 void init_cmpl_array(const int& gi0, const int& gi1, const int& gi2, std::complex<Float>& v){ v = {Float(gi0), Float(gi1)}; }
-
-template<class FFT>
-class DistributedFFT : protected FFT {
-public:
-    DistributedFFT(DecompInfo rd, DecompInfo cd, Array<typename FFT::RT>& ra, Array<typename FFT::CT>& ca):
-    FFT(rd, cd, ra.ptr, ca.ptr){
-    }
-};
 
 int main(int argc, char* argv[]){
     //int3 real_shape = {80, 80, 80};
@@ -47,47 +39,11 @@ int main(int argc, char* argv[]){
 
     Array<Float> arr_real(decomp_real);
     Array<Complex> arr_cmpl(decomp_cmpl);
-    //Array<Complex> arr3(decomp_cmpl);
 
-
-    //typedef  BaseClass;
-    //DistributedFFT<Float, BaseClass> fft(decomp_real, decomp_cmpl, arr_real, arr_cmpl);
     DistributedFFT<typename FFTW::FFT<Float> > fft(decomp_real, decomp_cmpl, arr_real, arr_cmpl);
 
-    //arr1.over(init_array);
-    //arr2.over(init_cmpl_array);
-    //arr3.over(init_cmpl_array);
-
-    ////arr1.save("real.bin");
-    //arr2.save("cmpl1.bin");
-
-
-    //for(int i = 0; i < 10; i++){
-    //arr2.set_x_pencil();
-    //arr3.set_y_pencil();
-    //arr2.transpose_into(arr3);
-    //arr2.set_z_pencil();
-    //arr3.transpose_into(arr2);
-
-    //arr2.transpose_into(arr3);
-    //arr2.set_x_pencil();
-    //arr3.transpose_into(arr2);
-    //}
-    //for(int i = 0; i < 100; i++){
-        //arr2.transpose_into(arr3);
-    //}
-
-    //arr3.set_x_pencil();
-    //arr2.set_y_pencil();
-    //arr2.transpose_into(arr3);
-
-    //arr3.transpose_into(arr2);
-
-    //arr2.set_z_pencil();
-    //arr3.transpose_into(arr2);
-
-    //arr2.save("cmpl2.bin");
-
+    arr_real.over(init_array);
+    arr_cmpl.over(init_cmpl_array);
 
      if(bk.rank == 0) std::cerr << "program terminating" << std::endl;
 
